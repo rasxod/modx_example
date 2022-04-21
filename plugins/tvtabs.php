a2a165c527f99ef692e14973cfabd97cf2dbd192
Nezabor <in001@z1q.ru>
2022-04-21 16:40:10 +0300

    Update tvtabs.php
    исправлена ошибка уникальности вкладок

--- plugins/tvtabs.php
+++ plugins/tvtabs.php
@@ -10,19 +10,23 @@ switch($eventName) {
         if( $resource && $resource->get('template') == 1 || !$resource){
             $tv_ids = $modx->getCollection('modTemplateVarTemplate', ['templateid' => $resource->get('template')]);
             foreach ($tv_ids as $key => $ti) {
-                $js_add = '';
+                $js_tab = [];
                 $_tv = $modx->getObject('modTemplateVar', ['id' => $ti->get('tmplvarid')]);
                 if ($_tv != null) {
                     $cat = $modx->getObject('modCategory', ['id' => $_tv->get('category')]);
                     if ($cat != null) {
                         $generator = $modx->newObject('modResource');
-                        $js_add .= '
-                            MODx.addTab("modx-resource-tabs",{title:"'.$cat->get('category').'",id:"'.$generator->cleanAlias($cat->get('category')).'"});
-                            MODx.moveTV(["tv'.$ti->get('tmplvarid').'"], "'.$generator->cleanAlias($cat->get('category')).'");
+                            $js_tab[$generator->cleanAlias($cat->get('category'))]['tab'] = 'MODx.addTab("modx-resource-tabs",{title:"'.$cat->get('category').'",id:"'.$generator->cleanAlias($cat->get('category')).'"});';
+                            $js_tab[$generator->cleanAlias($cat->get('category'))]['tv'][] = 'MODx.moveTV(["tv'.$ti->get('tmplvarid').'"], "'.$generator->cleanAlias($cat->get('category')).'");
                         ';
                     }
                 }
             }
+            $js_add = '';
+            foreach ($js_tab as $key => $jst) {
+                $js_add .= $jst['tab'].implode(' ', $jst['tv']);
+            }
+
             $modx->regClientStartupHTMLBlock('<script>
         Ext.onReady(function() {
             	
